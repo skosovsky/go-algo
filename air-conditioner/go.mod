2402dcb312c9a97de82a5c5c8feb85acba433d16
Sergey Kosovsky <skosovsky@gmail.com>
2023-10-31 16:45:58 +0300

    Add solution for air-conditioner

---
+++ air-conditioner/go.mod
@@ -0,0 +1,3 @@
+module github.com/skosovsky/algo.git/air-conditioner
+
+go 1.21.3
---
+++ air-conditioner/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tempRoom, tempCond int
+	var modeCond string
+	fmt.Scan(&tempRoom, &tempCond) // standard input
+	fmt.Scan(&modeCond)
+
+	fmt.Print(detectTemp(tempRoom, tempCond, modeCond)) // standard output
+}
+
+func detectTemp(tempRoom int, tempCond int, modeCond string) int {
+	switch modeCond {
+	case "freeze":
+		if tempCond >= tempRoom {
+			return tempRoom
+		}
+		return tempCond
+	case "heat":
+		if tempCond <= tempRoom {
+			return tempRoom
+		}
+		return tempCond
+	case "auto":
+		return tempCond
+	case "fan":
+		return tempRoom
+	default:
+		return tempRoom
+	}
+}
