e189ff124816bac3968fd2d47df7e5307838fcba
Sergey Kosovsky <skosovsky@gmail.com>
2024-03-21 16:38:06 +0300

    Add check bill number

--- me-sms/main.go
+++ me-sms/main.go
@@ -13,13 +13,14 @@ type Message struct {
 
 func main() {
 	sms := []Message{
-		{Content: "СЧЁТ7999 10:02 Перевод 350р от Ирина С. Баланс: 1150.28р"},
-		{Content: "Перевод из Тинькофф Банк +7968р от АЛЕКСАНДР М. СЧЁТ7999 — Баланс: 9933.28р"},
-		{Content: "Перевод из Газпромбанк +2778р от КИРИЛЛ П. СЧЁТ3550 — Баланс: 18044.43р"},
-		{Content: "СЧЁТ3550 19:25 Перевод 10 790р от Людмила Д. Баланс: 15 266.43р"},
-		{Content: "MIR-9550 13:35 перевод 400р Комиссия 2р TINKOFF Баланс: 18 062.43р"},
-		{Content: "MIR-9550 13:38 Покупка 75.20р Urent Баланс: 17 987.23р"},
-		{Content: "MIR-9550 17:26 Покупка 754р APTEKA Баланс: 18 396.23р"},
+		{Content: "MIR-2778 10:02 Перевод 350р от Ирина С. Баланс: 1150.28р"},
+		{Content: "Перевод из Тинькофф Банк +7968р от АЛЕКСАНДР М. MIR-2778 — Баланс: 9933.28р"},
+		{Content: "Перевод из Газпромбанк +2778р от КИРИЛЛ П. MIR-2778 — Баланс: 18044.43р"},
+		{Content: "MIR-2778 19:25 Перевод 10 790р от Людмила Д. Баланс: 15 266.43р"},
+		{Content: "MIR-2778 13:35 перевод 400р Комиссия 2р TINKOFF Баланс: 18 062.43р"},
+		{Content: "MIR-2778 13:38 Покупка 75.20р Urent Баланс: 17 987.23р"},
+		{Content: "MIR-2778 17:26 Покупка 754р APTEKA Баланс: 18 396.23р"},
+		{Content: "MIR-9550 13:18 Перевод 2650р от Элеонора К. Баланс: 19 579.59р"},
 	}
 
 	for _, v := range sms {
@@ -32,7 +33,11 @@ func NewSms(sms Message) {
 	log.Println("new sms -", sms.Content)
 
 	if !strings.Contains(strings.ToLower(sms.Content), "перевод") {
-		log.Println("В теле сообщения отсутствует информация о переводе")
+		log.Println("отсутствует информация о переводе")
+		return
+	}
+	if !strings.Contains(strings.ToUpper(sms.Content), "MIR-2778") {
+		log.Println("отсутствует информация о нужном счете")
 		return
 	}
 
@@ -41,7 +46,7 @@ func NewSms(sms Message) {
 	f := r.FindAllStringSubmatch(sms.Content, -1)
 
 	if f == nil || len(f[0]) == 0 {
-		log.Println("В теле сообщения отсутствует сумма")
+		log.Println("отсутствует сумма")
 		return
 	}
 
@@ -49,9 +54,9 @@ func NewSms(sms Message) {
 	amount, err := strconv.ParseFloat(f[0][1], 64)
 
 	if err == nil {
-		log.Println("Идентификация суммы", amount)
+		log.Println("идентификация суммы", amount)
 	} else {
-		log.Println("Сумма не идентифицирована")
+		log.Println("сумма не идентифицирована")
 		return
 	}
 }
